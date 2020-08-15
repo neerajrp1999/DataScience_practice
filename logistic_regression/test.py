4279e758757fbc367990809cb94d94f92fd2be97
neerajuchiha <neerajrp1999@gmail.com>
2020-08-15 20:23:52 +0530

    Update test.py

--- logistic_regression/test.py
+++ logistic_regression/test.py
@@ -30,7 +30,7 @@ y_pred = model.predict(x_test)
 from sklearn.metrics import confusion_matrix
 c=confusion_matrix(y_pred, y_test)
 print(c)
-x_set, y_set = x_train, y_train
+x_set, y_set = x_test, y_test
 
 from matplotlib.colors import ListedColormap
 
@@ -42,14 +42,14 @@ for i, j in enumerate(np.unique(y_set)):
     plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
 plt.show()
+
 #train
-x_set, y_set = x_test, y_test
+x_set, y_set = x_train, y_train
 x_1,x_2=np.meshgrid(np.arange(start=x_train[:,0].min()-1,stop=x_train[:,0].max()+1,step=0.001),
                     np.arange(start=x_train[:,1].min()-1,stop=x_train[:,1].max()+1,step=0.001))
 plt.contour(x_1,x_2,model.predict(np.array([x_1.ravel(),x_2.ravel()]).T).reshape(x_1.shape),cmap = ListedColormap(('red', 'green')))
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-
 plt.show()
 
