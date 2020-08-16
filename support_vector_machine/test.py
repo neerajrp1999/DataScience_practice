64c5cff3143b3801da9612ddf5f0db1a09846cf7
neerajuchiha <neerajrp1999@gmail.com>
2020-08-16 17:23:16 +0530

    Update test.py

--- support_vector_machine/test.py
+++ support_vector_machine/test.py
@@ -3,20 +3,16 @@
 """
 Created on Sun Aug 16 17:17:16 2020
 
-@author: thispc
+@author: Neeraj Prajapati
 """
 
-# Support Vector Machine (SVM)
-
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
-dataset = pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, [2, 3]].values
-y = dataset.iloc[:, -1].values
+file = pd.read_csv('Social_Network_Ads.csv')
+X = file.iloc[:, [2, 3]].values
+y = file.iloc[:, -1].values
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
